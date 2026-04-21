5edc03f6dbe057a1cf9c6807052717760a705b8f
tom <you@example.com>
2026-04-21 13:26:21 +0530

    inital commit

--- src/main/java/com/sbi/lms/controller/ApplicationController.java
+++ src/main/java/com/sbi/lms/controller/ApplicationController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 //hi
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.list;
 
 /**
  * Loan Application REST endpoints.
