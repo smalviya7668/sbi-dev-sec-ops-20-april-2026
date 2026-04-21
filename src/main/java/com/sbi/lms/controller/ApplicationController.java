79771de70cad0c1ef77e99b89b8f110f8fc87205
tom <you@example.com>
2026-04-21 13:29:12 +0530

    inital commit

--- src/main/java/com/sbi/lms/controller/ApplicationController.java
+++ src/main/java/com/sbi/lms/controller/ApplicationController.java
@@ -51,7 +51,7 @@ public class ApplicationController {
                                   SecurityService securityService) {
         this.applicationService = applicationService;
         this.securityService    = securityService;
-        System.out.println("heloooo")
+        System.out.println("heloooo");
     }
 
     @GetMapping
